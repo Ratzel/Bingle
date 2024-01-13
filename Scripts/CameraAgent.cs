92a9cad3c09359d2473860b307b423956465bdf6
Mountain_Home <pure.innovator@gmail.com>
2024-01-13 16:51:14 +0900

    빙글 프로젝트 생성 - 사용 폴더 및 카메라 기초 세팅 진행

---
+++ Scripts/CameraAgent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAgent : MonoBehaviour
+{
+    [SerializeField] Camera m_TargetCamera;
+    [SerializeField] float m_BoardUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //해상도 변경에 대응
+        //m_BoardUnit의 외부 입력값을 이용하여 실행시 orthographicSize를 재계산하여 대응)
+        m_TargetCamera.orthographicSize = m_BoardUnit / m_TargetCamera.aspect;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
